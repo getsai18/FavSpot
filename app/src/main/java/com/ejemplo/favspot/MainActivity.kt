9378d10e3deb738dd3fa56d775278038ce28c3aa
Max432122 <20243ds175@utez.edu.mx>
2025-12-05 20:07:45 -0600

    Arregle el MainActivity.kt

--- app/src/main/java/com/ejemplo/favspot/MainActivity.kt
+++ app/src/main/java/com/ejemplo/favspot/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.ejemplo.favspot.navigation.AppNavigation
 import com.ejemplo.favspot.ui.theme.FavSpotTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +19,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            FavSpotTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            AppNavigation()
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FavSpotTheme {
-        Greeting("Android")
-    }
-}

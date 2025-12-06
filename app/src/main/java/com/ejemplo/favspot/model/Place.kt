9276944d8fe0567b3ad90d0518ebb83851024f6d
getsai18 <20243ds167@utez.edu.mx>
2025-12-05 19:38:05 -0600

    Agrege la clase Place

---
+++ app/src/main/java/com/ejemplo/favspot/model/Place.kt
@@ -0,0 +1,25 @@
+package com.ejemplo.favspot.model
+
+import com.google.gson.annotations.SerializedName
+
+class Place {
+    val id: Int,
+
+    @SerializedName("user_id")
+    val userId: Int,
+
+    @SerializedName("name")
+    val name: String,
+
+    @SerializedName("latitude")
+    val latitude: Double,
+
+    @SerializedName("longitude")
+    val longitude: Double,
+
+    @SerializedName("notes")
+    val notes: String? = "",
+
+    @SerializedName("image_url")
+    val imageUrl: String? = null
+}
